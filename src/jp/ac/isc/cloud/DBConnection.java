04d3cfc445ddc42c8a3d50ed4a8a723e45d28023
n1201006 <cahh1201006@gn.iwasaki.ac.jp>
2021-07-29 11:19:52 +0900

    練習02_3_deploy

--- src/jp/ac/isc/cloud/DBConnection.java
+++ src/jp/ac/isc/cloud/DBConnection.java
@@ -6,11 +6,34 @@ public final class DBConnection {
 	private DBConnection() {}
 
 	public static Connection openConnection() {
+
+		String connectString = System.getenv("MYSQLCONNSTR_localdb");
+		String database = "";
+		String port = "";
+		String username = "";
+		String password = "";
+
+		String[] strArray = connectString.split(";");
+
+		for(int i = 0; i < strArray.length; i++) {
+			String[] paramArray = strArray[i].split("=");
+			switch(i) {
+			case 0: database = paramArray[1];
+			continue;
+			case 1: port = paramArray[1];
+			continue;
+			case 2: username = paramArray[1];
+			continue;
+			case 3: password = paramArray[1];
+			continue;
+			}
+		}
 		Connection users = null;
 
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			users = DriverManager.getConnection("jdbc:mysql://localhost/servlet_db?useUnicode=true&characterEncoding=utf8", "root", "");
+	//		users = DriverManager.getConnection("jdbc:mysql://localhost/servlet_db?useUnicode=true&characterEncoding=utf8", "root", "");
+			users = DriverManager.getConnection("jdbc:mysql://" + port + "/" + database + "?useUnicode=true&characterEncoding=utf8",username,password);
 		}catch(SQLException e){
 			e.printStackTrace();
 		}catch(ClassNotFoundException e) {
